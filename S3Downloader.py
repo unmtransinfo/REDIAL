e711e35b1b7e88793800c774ead0ba047f1fa35f
Priyansh Kedia <priyansh-kedia@Priyanshs-MBP-2.ht.home>
2023-07-25 15:13:55 -0600

    changes in s3downloader

--- S3Downloader.py
+++ S3Downloader.py
@@ -99,7 +99,7 @@ class S3Downloader(metaclass = Singleton):
 
     def download_dir(self, key):
         objs = s3.list_objects_v2(Bucket = BUCKET, Prefix = key)
-        for obj in objs:
-            print(obj.Contents)
-            obj_key = obj['Contents']['Key']
+        for obj in objs['Contents']:
+    
+            obj_key = obj['Key']
             self.download_file(obj_key)
