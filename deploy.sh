ee2ce99874bcb6ea0b2a33b746803ebb137dcf51
Priyansh-Kedia <kediapriyansh@gmail.com>
2023-08-07 01:52:51 -0600

    minor change in docker compose

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 #!/bin/bash
 
-docker-compose -f docker-compose.prod-init.yml up --build -d 
+docker-compose -f docker-compose.prod-init.yml --env-file env-cert.prod up --build -d 
 docker-compose -f docker-compose-prod.yml --env-file .env.prod up --build -d
