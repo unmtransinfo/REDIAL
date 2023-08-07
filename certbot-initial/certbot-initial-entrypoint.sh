7bceed5403139adebdfd103b6ed92b2980210190
Priyansh-Kedia <kediapriyansh@gmail.com>
2023-08-07 02:30:40 -0600

    minor change in dockerfile

--- certbot-initial/certbot-initial-entrypoint.sh
+++ certbot-initial/certbot-initial-entrypoint.sh
@@ -4,8 +4,8 @@ certbot certonly \
         -m $EMAIL_ADDRESS \
         --rsa-key-size "2048" \
         --agree-tos \
-        -n
-        --server https://acme-staging-v02.api.letsencrypt.org/directory
+        -n \
+        --server https://acme-staging-v02.api.letsencrypt.org/directory \
         --staging
         # --force-renewal
         # -vvv
