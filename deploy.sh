8696fb65bb233de9dc909dcd419d0c01d5191832
Priyansh-Kedia <kediapriyansh@gmail.com>
2023-08-07 01:39:16 -0600

    minor change in docker compose

--- deploy.sh
+++ deploy.sh
@@ -2,24 +2,5 @@
 
 #!/bin/bash
 
-LETSENCRYPT_EMAIL="pkedia@salud.unm.edu"
-DNSNAME="unmtid-services.net"
-
-docker run -it --rm --name certbot \
-    -v "$PWD/letsencrypt:/etc/letsencrypt" \
-    -v "$PWD/lib/letsencrypt:/var/lib/letsencrypt" \
-    certbot/certbot \
-    certonly \
-    -m $LETSENCRYPT_EMAIL \
-    --manual \
-    --preferred-challenges dns-01 \
-    --no-eff-email \
-    --manual-public-ip-logging-ok \
-    --keep-until-expiring \
-    --agree-tos \
-    -d $DNSNAME \
-    --server https://acme-v02.api.letsencrypt.org/directory
-
-docker-compose -f docker-compose-prod-init.yml --env-file .env-cert.prod up --build -d
-
+docker-compose -f docker-compose.prod-init.yml up --build -d 
 docker-compose -f docker-compose-prod.yml --env-file .env.prod up --build -d
