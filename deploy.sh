c1b81324d04bb74389ca3b3c9efbc43a2984f00d
Priyansh-Kedia <kediapriyansh@gmail.com>
2023-08-07 01:28:44 -0600

    minor change in docker compose

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,6 @@ docker run -it --rm --name certbot \
     -d $DNSNAME \
     --server https://acme-v02.api.letsencrypt.org/directory
 
+docker-compose -f docker-compose-prod-init.yml --env-file .env-cert.prod up --build -d
+
 docker-compose -f docker-compose-prod.yml --env-file .env.prod up --build -d
